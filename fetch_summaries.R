bd4b718157fa2c5880d6aa3df37af33b85492c7b
Eric Marty <allopole@noreply.github.com>
2020-03-31 15:26:14 -0400

    insert iframe resizing script on download

--- fetch_summaries.R
+++ fetch_summaries.R
@@ -1,18 +1,35 @@
+# function to fetch summaries and insert code
+
+fetch_and_fix <- function(url,destfile) {
+  # tmp <- readLines(url)
+  tmp <- tempfile("tmp", fileext = ".html")
+  utils::download.file(url, destfile = tmp, cacheOK = FALSE)
+  
+  # insert resizer script
+  script <- '<script type="text/javascript" src="js/iframeResizer.contentWindow.min.js"></script>'
+  h <- readLines(tmp)
+  out <- gsub("</body>",paste0(script,"\n</body>"),h)
+  
+  # insert resizer script
+  writeLines(out,destfile)
+  cat(paste("Done writing", destfile,"\n"))
+}
+
 # fetch summaries
 
 # Early Intervention - web summary
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-early-intervention/master/web-summary.html?token=ADVPF4FUMEJR3Y7T33D7Y226Q5KR6",
   destfile="early-intervention-web-summary.html"
 )
 
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-early-intervention/master/web-summary2.html?token=ADVPF4A7VAC5CX3EYYLKTIK6Q5K3O",
   destfile="early-intervention-web-summary2.html"
 )
 
 # Data Summary - China
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-data-summary/master/nCov-Mapping-China-Web-Summary.html?token=ADVPF4H54WWXLYTXPSBATHK6QSMTM",
   destfile="nCov-Mapping-China-Web-Summary.html"
 )
@@ -20,31 +37,31 @@ utils::download.file(
 # https://raw.githubusercontent.com/CEIDatUGA/ncov-data-summary/master/nCov-Mapping-China.html?token=ADVPF4F5YXAKG7L3L7D6RL26JC6V2
 
 # Data Summary - World
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-data-summary/master/nCov-Mapping-US-Web-Summary.html?token=ADVPF4AKIDH3IKQOS4VD2AC6QSM32",
   destfile="nCov-Mapping-World-Web-Summary.html"
 )
 
 # Data Summary - US
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-data-summary/master/nCov-Mapping-US-Web-Summary.html?token=ADVPF4HQGE2PCJAXJTS2CJC6QSNAC",
   destfile="nCov-Mapping-US-Web-Summary.html"
 )
 
 # Data Summary - Epidemic Curves
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-data-summary/master/epidemic-curves-web-summary.html?token=ADVPF4HCYWWSCX7XXHWCRKS6QSNG6",
   destfile="epidemic-curves-web-summary.html"
 )
 
 # Speed of Spread - Epidemic Curves by Country
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-data-summary/master/epi-curve-by-country-web-summary.html?token=ADVPF4HXCTP5ZCOOVOACTGK6QNAPK",
   destfile="epi-curve-by-country-web-summary.html"
 )
 
 # Speed of Spread - Epidemic Curves by US State
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-data-summary/master/epi-curve-by-us-state-web-summary.html?token=ADVPF4BDC6IGU5NDQ6LGMJK6QNAR4",
   destfile="epi-curve-by-us-state-web-summary.html"
 )
@@ -52,52 +69,52 @@ utils::download.file(
 
 
 # Context
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-context/master/web-summary.html?token=ADVPF4BX64JLSPHNAA6GL5K6QSNKU",
   destfile="context-web-summary.html"
 )
 
 # Epidemic Characteristics
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-parameters/master/web-summary.html?token=ADVPF4E7DI66NH7EBVQDKWK6RHIME",
   destfile="parameters-web-summary.html"
   )
 
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-parameters/master/ncov-parameters.html?token=ADVPF4EC2XFZGQZY4IY4CW26RHIPS",
   destfile="parameters-supplement.html"
 )
 
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-parameters/master/parameters-US.html?token=ADVPF4DFB2GWORZIJDP3XJK6RNYHW",
   destfile="parameters-US.html"
 )
 
 # R_eff outside China
 
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-Reff-outside-China/master/web-summary.html",
   destfile="reff-outside-web-summary.html"
 )
 
 # Stochastic Model
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-wuhan-stochastic-model/master/web-summary.html?token=ADVPF4G3KEESL744SSRW2JS6RRVJS",
   destfile="stochastic-web-summary.html"
 )
 
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-wuhan-stochastic-model/master/stochastic-model-georgia-web-summary.html?token=ADVPF4BFH3NJLM66KUGON2S6RRU26",
   destfile="stochastic-georgia-web-summary.html"
 )
 
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-wuhan-stochastic-model/master/stochastic-model.html?token=ADVPF4EK46ACYA6WBSXHPOC6QNBDM",
   destfile="stochastic-model.html"
 )
 
 # Mass Testing
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/CoV_MassTesting/master/COVID19_CEID_summary_html.html?token=ADVPF4DAFKOYA4M23ZEHOUS6Q5K6Q",
   destfile="mass_testing_summary.html"
 )
@@ -114,39 +131,40 @@ utils::download.file(
 
 
 # Nowcast US
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-nowcast/master/nowcast-US.html?token=ADVPF4GDLWVKSML4VYM65A26Q5LK4",
   destfile="nowcast-US.html"
 )
 
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-nowcast/master/nowcast-US-cases.html?token=ADVPF4DU63LK34X5SHHJMJC6Q5LMW",
   destfile="nowcast-US-cases.html"
 )
 
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-nowcast/master/nowcast-US-fatalities.html?token=ADVPF4CGOO44UTTLX33MHKK6Q5LPE",
   destfile="nowcast-US-fatalities.html"
 )
 
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-nowcast/master/under-reporting.html?token=ADVPF4DYMNTCVZOUW7BCNAS6Q5LHU",
   destfile="nowcast-US-ascertainment.html"
 )
 
+# Standalone nowcast plot for front page
 utils::download.file(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-nowcast/master/USnowcast_plot.html?token=ADVPF4GFXWRSI7IB7ACCTNS6RMNIK",
   destfile="USnowcast_plot.html"
 )
 
 # China Spatial Model
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/CoronavirusSpatial/master/web_summary.html?token=ADVPF4HNWWHNHPOLADS7UZ26QSNWW",
   destfile="china-spatial-web-summary.html"
 )
 
 # China Spatial Model - links map - UPDATE MANUALLY - FILE NAME CHANGES
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/CoronavirusSpatial/master/maps/link-weights-chloropleth-highest-ten-pct-2020-03-22.html?token=ADVPF4ADQMOTNJMM5L2AOVK6QSNTO",
   destfile="china-spatial-link-weights.html"
 )
@@ -158,13 +176,16 @@ utils::download.file(
 # )
 
 # Final Size - web summary
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-coupled-outbreaks/master/finalSizeSummary.html?token=ADVPF4HYL4HGX7MCAPC7KMS6QSNZG",
   destfile="final-size-web-summary.html"
 )
 
 # Final Size - supp
-utils::download.file(
+fetch_and_fix(
   url="https://raw.githubusercontent.com/CEIDatUGA/ncov-coupled-outbreaks/master/finalSize.html?token=ADVPF4C54IXPZETMXFEQP6K6QSN26",
   destfile="final-size-supplement.html"
 )
+
+
+
