15c2cea8cc908cbcdb8a8f7464ed4e1c4f28c535
Eric Marty <allopole@noreply.github.com>
2020-04-21 11:05:42 -0400

    Add people images converted to jpeg

---
+++ convert_images.R
@@ -0,0 +1,10 @@
+
+path <- as.list(paste0("images/people/",list.files("images/people/")))
+
+webp_to_jpg <- function(path) {
+  image_read(path) %>% 
+    image_convert("jpg") %>% 
+    image_write(path = gsub("webp","jpg",path), format = "jpg")
+}
+
+lapply(path, webp_to_jpg)
