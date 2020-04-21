9711a1638e3f2054a02afa78cde69d2181e59c9b
Eric Marty <allopole@noreply.github.com>
2020-04-21 16:06:29 -0400

    update banner images

--- convert_images.R
+++ convert_images.R
@@ -1,6 +1,8 @@
 
 path <- as.list(paste0("images/people/",list.files("images/people/")))
 
+path <- "images/Coronavirus-working-group.webp"
+
 webp_to_jpg <- function(path) {
   image_read(path) %>% 
     image_convert("jpg") %>% 
