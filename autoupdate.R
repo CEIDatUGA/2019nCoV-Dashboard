024c7b8de24afb55a962539b1afba65836401ab4
Eric Marty <allopole@noreply.github.com>
2020-06-15 23:09:04 -0400

    automated daily update test

---
+++ autoupdate.R
@@ -0,0 +1,8 @@
+# script to automatically render site from crontab (MacOS)
+
+library(rmarkdown)
+
+# pandoc location must be hard coded. Find with Sys.getenv("RSTUDIO_PANDOC")
+Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
+
+rmarkdown::render_site()
